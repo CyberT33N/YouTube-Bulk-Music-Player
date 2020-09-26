31dcf4f4436aafc027213ed4aef0a99acb359739
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-09-26 21:51:49 +0200

    Delete install_module.sh

--- install_module.sh
+++
@@ -1,9 +0,0 @@
-#pwd <-- current directory
-#-------------------------------------------------------------------------------
-cd "$(dirname "$0")"
-pwd
-printf "\nWe will display now the current directory used:"
-echo "$(dirname "$0")"
-printf "\n\nWe will start now your bot!\n\nPlease wait.. This maybe take some time..\n"
-npm i fancy-log chalk puppeteer chalk-animation gradient-string cheerio unq
-printf "\nWe finished the .sh file :) - Created by Dennis Demand( https://github.com/CyberT33N )\n"
