c6bff161895dd3995947489d574bb415dc0648e5
CyberT33N <cybert33n@gmail.com>
2022-05-26 14:39:38 +0200

    refactor(CCS-001): backup

--- app.js
+++ app.js
@@ -20,82 +20,54 @@
 |__|__|_|  /   __/ \____/|__|   |__|   |   __/|____/____/\___  /|__|___|  /____  >
          \/|__|                        |__|             /_____/         \/     \/
 */
+'use strict'
+
 var ytLinks_AR;
 
-             const fs = require('fs'),
-                  log = require('fancy-log'),
-                chalk = require('chalk'),
-       chalkAnimation = require('chalk-animation'),
-             gradient = require('gradient-string'),
+const log = require('fancy-log')
+const chalk = require('chalk')
+const gradient = require('gradient-string')
 
-           controller = require('./controller/controller'),
-        controllerLib = require('./controller/controllerLib');
+const controller = require('./controller/controller')
+const controllerLib = require('./controller/controllerLib')
 
+// ADVERTISE
+const ads = gradient('red', 'white').multiline([
+      '',
+      'Presented by',
+      '████████╗██████╗ ██████╗ ███╗   ██╗',
+      '╚══██╔══╝╚════██╗╚════██╗████╗  ██║',
+      '   ██║    █████╔╝ █████╔╝██╔██╗ ██║',
+      '   ██║    ╚═══██╗ ╚═══██╗██║╚██╗██║',
+      '   ██║   ██████╔╝██████╔╝██║ ╚████║',
+      '   ╚═╝   ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝ Software'
+].join('\n'));
+console.log(ads);
+console.log( '\nCheck my Github Profile: ' + chalk.white.bgGreen.bold(' github.com/CyberT33N ')  + '\n\n');
+console.log( gradient('white', 'black')('\n\n=======================================\n\n') );
 
-
-
-
-
-
-
-
-
-   // ADVERTISE
-   var ads = gradient('red', 'white').multiline([
-          '',
-          'Presented by',
-          '████████╗██████╗ ██████╗ ███╗   ██╗',
-          '╚══██╔══╝╚════██╗╚════██╗████╗  ██║',
-          '   ██║    █████╔╝ █████╔╝██╔██╗ ██║',
-          '   ██║    ╚═══██╗ ╚═══██╗██║╚██╗██║',
-          '   ██║   ██████╔╝██████╔╝██║ ╚████║',
-          '   ╚═╝   ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝ Software'
-   ].join('\n'));
-   console.log(ads);
-   console.log( '\nCheck my Github Profile: ' + chalk.white.bgGreen.bold(' github.com/CyberT33N ')  + '\n\n');
-   console.log( gradient('white', 'black')('\n\n=======================================\n\n') );
-
-
-
-
-
-
-
-log( 'Current working directory: ' + __dirname );
 (async () => {
-log( 'ENTER main async area..' );
+    log( 'Current working directory: ' + __dirname );
+    log( 'ENTER main async area..' );
 
+    // get youtube links from bookmarks.txt
+    ytLinks_AR = await controllerLib.parseYoutTubeLinks();
+    if(!ytLinks_AR) throw new Error('No video was able to get parsed..');
+    log( 'We successfully parsed all YouTube links.. ytLinks_AR[0]: ' + ytLinks_AR[0] );
 
+    // start browser and get page & client
+    const botEngine_AR = await controller.startBROWSER();
+    log( 'startBROWSER() done..' );
 
+    if(!botEngine_AR) throw new Error('Something went wrong we cant find botEngine_AR');
+    const client = botEngine_AR.client;
+    const page = botEngine_AR.page;
 
-        // get youtube links from bookmarks.txt
-        ytLinks_AR = await controllerLib.parseYoutTubeLinks();
-        if(!ytLinks_AR) throw new Error('No video was able to get parsed..');
-        log( 'We successfully parsed all YouTube links.. ytLinks_AR[0]: ' + ytLinks_AR[0] );
-
-
-
-
-
-        // start browser and get page & client
-        const botEngine_AR = await controller.startBROWSER();
-        log( 'startBROWSER() done..' );
-
-        if(!botEngine_AR) throw new Error('Something went wrong we cant find botEngine_AR');
-        const client = botEngine_AR.client;
-        const page = botEngine_AR.page;
-
-
-
-
-       await startYoutTube(ytLinks_AR, client, page);
-       log( 'finish..' );
-
-
-
-
-
-})().catch((e) => {  log('ASYNC - MAIN - error :' + e + '\nytLinks_AR[0]: ' + ytLinks_AR[0] ); });
+   await startYoutTube(ytLinks_AR, client, page);
+   log( 'finish..' );
+})().catch((e) => {
+    log('ASYNC - MAIN - error :' + e + '\nytLinks_AR[0]: ' + ytLinks_AR[0] )
+})
 
 
 
